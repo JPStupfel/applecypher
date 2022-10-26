b4558281d59cbec4ef99f78eeb5754470006be39
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-10-26 13:17:47 -0500

    removed pro routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   
   resources :posts
   resources :proposals, only: [:index, :create, :show, :destroy, :update]
-  resources :bids, only: [:index, :create]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
  #test
@@ -19,10 +18,7 @@ Rails.application.routes.draw do
   delete '/session', to: "sessions#destroy"
   get '/me', to: "sessions#index"
 # routes for profesional authenication
-  post '/sessionPro', to: "session_pros#create"
-  patch '/sessionPro', to: "session_pros#update"
-  delete '/sessionPro', to: "session_pros#destroy"
-  get '/mePro', to: "session_pros#index"
+  
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
