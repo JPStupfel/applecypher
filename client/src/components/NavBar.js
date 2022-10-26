92ac6fc0be2a3287cbbd4f008e81528283bb2c67
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-10-26 16:12:18 -0500

    logging out now redirects to places

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,32 +1,27 @@
 import React from 'react'
+import {useNavigate}  from 'react-router-dom'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useSelector } from 'react-redux';
 
 function NavBar({handleLogout}) {
+  const history = useNavigate();
   const user = useSelector(state=>state)
+  function handleClickLogout(event){
+    handleLogout();
+    history('/places')
+  }
 
   return (
     <>
-
       <Navbar bg="dark" variant="dark">
         <Container>
           <Nav className="me-auto">
-
-          
           <Nav.Link href="/#/places">Browse Places</Nav.Link> 
-         
-          {user.user_type === 'Client' ? <Nav.Link href="/#/new-place">Create Place</Nav.Link> : null}
-          
-
+            {user.user_type === 'Client' ? <Nav.Link href="/#/new-place">Create Place</Nav.Link> : null}
           </Nav>
         </Container>
-
-
-
-    
-       
         {user.id ?
          <div className='navbar-image-container'>
           <button onClick={handleLogout}>Logout {user.username}</button>
@@ -36,13 +31,9 @@ function NavBar({handleLogout}) {
          <Nav>
         <Nav.Link href="/#/login">Login</Nav.Link>
         <Nav.Link href="/#/signup">Signup</Nav.Link>
-
         </Nav>
         }
-
       </Navbar>
-      
-      
     </>
   );
 }
