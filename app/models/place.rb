122afc8a5c5140339cdd659690476baed316c549
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-10-30 15:18:17 -0500

    if self.pictures is false return dummy image

--- app/models/place.rb
+++ app/models/place.rb
@@ -7,7 +7,7 @@ class Place < ApplicationRecord
     validates :description, presence: true
 
     def first_picture
-       url = self.pictures.first.imagable.url ? self.pictures.first.imagable.url : 'https://i.seadn.io/gae/w4aZjvr6dpPfkSqSZjiYodI_zFawDfu4e3SnqK27_t9EPp7KXPcPhCSrhMjLprJxPP8CRopj_PIr0z-J5H5ImimJxidRmxkBppiU?auto=format&w=384'
+       url = self.pictures ? self.pictures.first.imagable.url : 'https://i.seadn.io/gae/w4aZjvr6dpPfkSqSZjiYodI_zFawDfu4e3SnqK27_t9EPp7KXPcPhCSrhMjLprJxPP8CRopj_PIr0z-J5H5ImimJxidRmxkBppiU?auto=format&w=384'
        return url
     end
 
