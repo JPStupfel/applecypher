7471a71f87711cadf491e6101c036138fd30cffd
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-10-30 06:46:29 -0500

    added fallback image to first_picture in Place.rb

--- app/serializers/place_pictures_serializer.rb
+++ app/serializers/place_pictures_serializer.rb
@@ -1,6 +1,6 @@
 class PlacePicturesSerializer < ActiveModel::Serializer
 
-  attributes :id, :title, :description, :client_id, :lat, :lng, :first_picture
+  attributes :id, :title, :description, :client_id, :lat, :lng
   has_many :pictures 
 
 end
