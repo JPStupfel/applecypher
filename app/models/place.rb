a5da3f95ac1b4424f68874e337c4416a2f844c3b
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-10-30 15:51:54 -0500

    first-image checks if array is greater than zero

--- app/models/place.rb
+++ app/models/place.rb
@@ -7,10 +7,10 @@ class Place < ApplicationRecord
     validates :description, presence: true
 
     def first_picture
-       url = self.pictures ? self.pictures.first.imagable.url : 'https://i.seadn.io/gae/w4aZjvr6dpPfkSqSZjiYodI_zFawDfu4e3SnqK27_t9EPp7KXPcPhCSrhMjLprJxPP8CRopj_PIr0z-J5H5ImimJxidRmxkBppiU?auto=format&w=384'
+        url = self.pictures.count > 0 ? self.pictures.first.imagable.url : 'https://i.seadn.io/gae/w4aZjvr6dpPfkSqSZjiYodI_zFawDfu4e3SnqK27_t9EPp7KXPcPhCSrhMjLprJxPP8CRopj_PIr0z-J5H5ImimJxidRmxkBppiU?auto=format&w=384'
        return url
     end
 
 
-    
+
 end
