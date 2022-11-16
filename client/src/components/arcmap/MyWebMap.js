6b70317868389fe8e7d30e569a3865d23272f3be
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-11-15 23:04:05 -0600

    made Mapview a function command not a constant

--- client/src/components/arcmap/MyWebMap.js
+++ client/src/components/arcmap/MyWebMap.js
@@ -1,11 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer";
-import Multipoint from "@arcgis/core/geometry/Multipoint";
-import SpatialReference from "@arcgis/core/geometry/SpatialReference";
-
 import MapView from "@arcgis/core/views/MapView";
-import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
-import Graphic from "@arcgis/core/Graphic";
 import WebMap from "@arcgis/core/WebMap";
 import esriConfig from "@arcgis/core/config";
 
@@ -15,26 +10,25 @@ export default function MyWebMap({ placeList, mapStyles }) {
     lat: placeList.length ? placeList[0].lat : 0,
     lng: placeList.length ? placeList[0].lng : 0,
   };
-
+  // useEffect to initiate map
   useEffect(() => {
     if (mapDiv.current) {
       esriConfig.portalUrl = "https://jpstupfel.maps.arcgis.com";
-
-
+      // create the map
       const map = new WebMap({
         portalItem: {
           // autocasts as new PortalItem()
           id: "480baf2adf9947ed9a32d2873b0421be",
         },
       });
-
-      const view = new MapView({
+      // create the view (ie initial render of map)
+      new MapView({
         map,
         container: mapDiv.current,
         center: [defaultCenter.lng, defaultCenter.lat],
         zoom: 7,
       });
-console.log(placeList[0])
+      // feature desc for each point to display in geojson
       const pointFeature = placeList.map((e) => {
         const westLng = e.lng - 360;
         return {
@@ -47,27 +41,23 @@ console.log(placeList[0])
           properties: {
             prop0: "value0",
             title: e.title,
-            imageUrl: e.first_picture
+            imageUrl: e.first_picture,
           },
         };
       });
-
+      // geojson attributes with points features as features
       const geojson = {
         type: "FeatureCollection",
         features: pointFeature,
       };
-
       // create a new blob from geojson featurecollection
       const blob = new Blob([JSON.stringify(geojson)], {
         type: "application/json",
       });
-
       // URL reference to the blob
       const url = URL.createObjectURL(blob);
       console.log(pointFeature);
       // create new geojson layer using the blob urlg23
-      
-
       const clusterConfig = {
         type: "cluster",
         clusterRadius: "100px",
@@ -75,16 +65,16 @@ console.log(placeList[0])
         // the number of features comprised by the cluster
         popupTemplate: {
           title: "Cluster summary",
-          content: "This cluster represents {cluster_count} earthquakes.",
+          content: "This cluster represents {cluster_count} places.",
           fieldInfos: [
             {
               fieldName: "cluster_count",
               format: {
                 places: 0,
-                digitSeparator: true
-              }
-            }
-          ]
+                digitSeparator: true,
+              },
+            },
+          ],
         },
         clusterMinSize: "24px",
         clusterMaxSize: "60px",
@@ -92,7 +82,7 @@ console.log(placeList[0])
           {
             deconflictionStrategy: "none",
             labelExpressionInfo: {
-              expression: "Text($feature.cluster_count, '#,###')"
+              expression: "Text($feature.cluster_count, '#,###')",
             },
             symbol: {
               type: "text",
@@ -100,20 +90,17 @@ console.log(placeList[0])
               font: {
                 weight: "bold",
                 family: "Noto Sans",
-                size: "12px"
-              }
+                size: "12px",
+              },
             },
-            labelPlacement: "center-center"
-          }
-        ]
+            labelPlacement: "center-center",
+          },
+        ],
       };
-
       const layer = new GeoJSONLayer({
-        title: "Earthquakes from the last month",
+        title: "Awesome Places",
         url: url,
-
         featureReduction: clusterConfig,
-
         // popupTemplates can still be viewed on
         // individual features
         popupTemplate: {
@@ -128,14 +115,13 @@ console.log(placeList[0])
             color: "#69dcff",
             outline: {
               color: "rgba(0, 139, 174, 0.5)",
-              width: 5
-            }
-          }
-        }
+              width: 5,
+            },
+          },
+        },
       });
-
-
-      map.add(layer); // adds the layer to the map
+      // adds the layer to the map
+      map.add(layer); 
     }
   });
 
